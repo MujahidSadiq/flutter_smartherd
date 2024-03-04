89a7efba63ba7efc1dcd1a45f38be0ce6d61b5e7
Mujahid_Sadiq <subhanmustafai1234@gmail.com>
2024-03-04 22:04:16 +0500

    learn about text style ,

--- lib/app_screen/home.dart
+++ lib/app_screen/home.dart
@@ -13,23 +13,30 @@ class Home extends StatelessWidget {
         // width: 200.0, // it will not give impact beacuse ,container widget is
         // taking its parent MaterialApp widget height and width.
         // height: 100.0,
-        // margin: EdgeInsets.all(15.0 ),  // or use only(left:15.0,top:15.0) as 
-        // padding: EdgeInsets.all(15.0),  
+        // margin: EdgeInsets.all(15.0 ),  // or use only(left:15.0,top:15.0) as
+        // padding: EdgeInsets.all(15.0),
         child: Text(
           'flight ',
           textDirection: TextDirection.ltr,
+          style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 75.0,
+            //fontfamily
+            //fontstyle 
+            fontWeight:FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            color:Colors.white,
+          ),
         ),
       ),
     );
   }
 }
 
-
 /** to solve this issue we take such widget which have more priority to 
  * acquire height and width of its child widget , so thats why we will 
  * wrap container widget with center widget . 
  */
-
 
 /**
  * Now how to use margin and padding in flutter 
