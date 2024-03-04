57423be562f95dccdc94496a8bece41e37da3357
Mujahid_Sadiq <subhanmustafai1234@gmail.com>
2024-03-04 21:38:43 +0500

    exploring container , center , margin and padding

---
+++ lib/app_screen/home.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        // container is similar to div element
+        alignment: Alignment.center,
+        color: Colors.deepPurple,
+        // width: 200.0, // it will not give impact beacuse ,container widget is
+        // taking its parent MaterialApp widget height and width.
+        // height: 100.0,
+        // margin: EdgeInsets.all(15.0 ),  // or use only(left:15.0,top:15.0) as 
+        // padding: EdgeInsets.all(15.0),  
+        child: Text(
+          'flight ',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+}
+
+
+/** to solve this issue we take such widget which have more priority to 
+ * acquire height and width of its child widget , so thats why we will 
+ * wrap container widget with center widget . 
+ */
+
+
+/**
+ * Now how to use margin and padding in flutter 
+ */
+
+/**
+ * how to give the style to the text widget . 
+ */
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_smartherd/app_screen/first_screen.dart';
+import 'package:flutter_smartherd/app_screen/home.dart';
 
-void main() => runApp(const MyFlutterApp());   
-/*use of function expression */
+/*exploring widgets  */
 
-
-/*
-     when this statement is called it is going 
-     to the class and call build method 
-     in response build method return materialAp widget .on the screen we get materilaApp
-     widget . */
-    
-
-// organizing flutter code
-
-/* make another file for MyFlutterApp link with main.dart by importing its name */
-
-
-/*fourth day branch: adding logic to out code , how to call a function froma 
-widget .  */
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Exploring UI widgets ',
+      home: Home(),
+    ),
+  );
+}
