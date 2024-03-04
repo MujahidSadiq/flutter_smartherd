ac3dde8810b558429b451fa0dbc365ce183660cc
Mujahid_Sadiq <subhanmustafai1234@gmail.com>
2024-03-04 19:04:55 +0500

    learn about Scaffold , appbar , body and exploration of widget docomentation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    const Center(
-      child: Text(
-        'hello flutter ',
-        textDirection: TextDirection.ltr,
-      ),
-    ),
-  );
+  runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Flutter Application',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My first App'),
+        ),
+        body: const Material(
+          color: Colors.lightBlueAccent,
+          child: Center(
+            child: Text(
+              'hello flutter ',
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      )));
 }
